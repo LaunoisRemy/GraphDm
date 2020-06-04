e80e697932fe39a9b6d0919b7fbe69d453f600fe
Launois Remy <remy.launois@gmail.com>
2020-06-04 12:48:31 +0200

    Exo 4

--- src/main/java/Graph/Graph.java
+++ src/main/java/Graph/Graph.java
@@ -1,4 +1,4 @@
-package Graph.Q3;
+package Graph;
 
 
 
@@ -10,14 +10,14 @@ import java.util.*;
 /**
  * Class represent graph with a list of adjacency
  */
-public class GraphQ3 {
-    private HashMap<Integer, List<Pair<Integer,Integer>>> matrice;
-    private HashMap<Integer, Boolean> visited;
+public class Graph {
+    private final HashMap<Integer, List<Pair<Integer,Integer>>> matrice;
+    private final HashMap<Integer, Boolean> visited;
 
     /**
      * Constructor to null
      */
-    public GraphQ3() {
+    public Graph() {
         this.matrice = new HashMap<>();
         this.visited = new HashMap<>();
     }
@@ -36,19 +36,38 @@ public class GraphQ3 {
         }
 
     }
+
     /**
-     * Add a next to a node and add in the other way (node to next)
+     * Add a next to a node and add in the other way (node to next) without weight
      * If the node doesnt exist create it
      * If next already exist print msg
      * @param node represent the node where we want to add a next
      * @param next to add
      */
-    public boolean addNextToNode(int node,int next,int weight){
-        Pair<Integer, Integer> p_pair = new Pair<>(next,weight);
-        return addNextToNode(node,p_pair);
+    public boolean addNextToNodeNoWeight(int node, int next){
+        if(!this.matrice.containsKey(node)){
+            this.addNode(node);
+        }
+        List<Pair<Integer,Integer>> voisins = this.matrice.get(node);
+
+        boolean existe=voisins.stream().anyMatch(p -> p.getKey() == next);
+        if(!existe){
+            voisins.add(new Pair<>(next,1));
+            this.addNextToNodeNoWeight(next,node);
+            return true;
+        }else{
+            return false;
+        }
     }
 
-    public boolean addNextToNode(int node,Pair<Integer, Integer> p_pair){
+    /**
+     * Ajout de noeud avec un poids
+     * @param node
+     * @param p_pair
+     * @param oriented graph oriented or not?
+     * @return
+     */
+    public boolean addNextToNodeWithWeight(int node, Pair<Integer, Integer> p_pair,boolean oriented){
         if(!this.matrice.containsKey(node)){
             this.addNode(node);
         }
@@ -58,25 +77,187 @@ public class GraphQ3 {
                 .anyMatch(p -> p.getKey() == p_pair.getKey());
         if(!existe){
             voisins.add(p_pair);
-            Pair<Integer, Integer> new_pair = new Pair<>(node,p_pair.getValue());
+            if(!oriented){
+                Pair<Integer, Integer> new_pair = new Pair<>(node,p_pair.getValue());
+                this.addNextToNodeWithWeight(p_pair.getKey(),new_pair,false);
+            }
             return true;
         }else{
             return false;
         }
     }
 
-    @Deprecated
+
     /**
-     * Add a full list to a node
-     * If next already exist print msg
-     * @param node represent the node where we want to add a next
-     * @param nexts to add
+     *
+     * @param source
      */
-    public void addListNextToNode(int node, List<Integer> nexts)  {
-        for (int newNexts : nexts){
-            //addNextToNode(node,newNexts);
+    public void breadthFirstSearch(int source) {
+        Queue<Object> queue = new LinkedList<Object>();
+        queue.add(source);
+
+        this.visited.put(source,true);
+
+        while (queue.size() != 0) {
+            int current = (int) queue.poll();
+            // Tous les voisins
+            List<Pair<Integer,Integer>> voisins = matrice.get(current);
+            for (Pair<Integer,Integer> v : voisins) {
+                //Node neighborNode = v.getNeighbor();
+                if (!this.visited.get(v.getKey())) {
+                    this.visited.put(v.getKey(),true);
+
+                    queue.add(v.getKey());
+                }
+            }
         }
     }
+
+    /**
+     *
+     * @return
+     */
+    public int getNbConnectedComp() {
+        int nb = 0;
+        Set<Integer> keySet = this.matrice.keySet();
+        for (int v : keySet) {
+            if (!this.visited.get(v)) {
+                breadthFirstSearch(v);
+                nb += 1;
+            }
+        }
+        return nb;
+    }
+
+    /**
+     *
+     * @param nb
+     * @return
+     */
+    public boolean estCo(int nb){
+        return nb == 1;
+    }
+
+
+    /**
+     *
+     * @param source
+     * @return
+     */
+    public Pair<ArrayList<Integer>,Integer> prim(int source){
+        int nb = this.getNbConnectedComp();
+        if(!estCo(nb)){
+            System.out.println("Non connexe");
+            return null;
+        }
+        //Queue<Integer> queue = new LinkedList<>(matrice.keySet());
+        // L'arbre des sommets a parcourir
+        ArrayList<Integer> tree = new ArrayList<>();
+        // Tous les arcs dont le sommet n'est pas dans tree
+        ArrayList<Pair<Integer,Integer>> allEdges=new ArrayList<>();
+
+        int current =source;
+        tree.add(source);
+        // Poids total
+        int cost = 0;
+        //Tant que on n'a pas tous les sommets dans notre arbre
+        while (tree.size() != matrice.keySet().size()) { //TODO normalement bizzare
+            //System.out.println(tree.toString());
+            // Tous les voisins de current
+            List<Pair<Integer,Integer>> edges = matrice.get(current);
+            // On ajoute tous les voisins
+            boolean goodAdd =   allEdges.addAll(edges);
+            // On supprime ceux dont on a deja un lien
+            allEdges.removeIf(p -> tree.contains(p.getKey()));
+            assert goodAdd = false;
+
+            // On trouve le minimum des voisins
+            Optional<Pair<Integer, Integer>> min = allEdges.stream()
+                    .min(Comparator.comparing(Pair::getValue));
+
+            if(min.isPresent()){
+                //On ajoute le poids minimum
+                cost+=min.get().getValue();
+                //On ajoute le sommet dans notre arbre
+                tree.add(min.get().getKey());
+                //on change le prochain current
+                current=min.get().getKey();
+            }
+
+        }
+        return new Pair<>(tree,cost);
+    }
+
+    /**
+     *
+     * @param source
+     * @return
+     */
+    public HashMap<Integer,Integer> dijkstra (int source){
+
+        int poidsCourant = 0;
+        // liste des sommets non parcourus avec le poids du chemin pour y arriver
+        HashMap<Integer,Integer> poids = new HashMap<>();
+        //Liste des sommets parcourus
+        HashMap<Integer,Integer> list = new HashMap<>();
+        //La distance ini est de 0
+
+        poids.put(source,0);
+        // Tous les sommets
+        Set<Integer> keys = matrice.keySet();
+        //On met infini dans les chemins non parcourus
+        for (int k: keys
+        ) {
+            if(k!=source){
+                poids.put(k,Integer.MAX_VALUE);
+            }
+        }
+        int courant = source;
+        // Tant qu'on a pas parcourus tous les sommets
+        while (poids.size() != 0 ){
+            System.out.println("Le courant est : "+ courant);
+            //On recupere les voisins du courant
+            if(matrice.get(source).size() == 0){
+                System.out.println("Le sommet n'a pas de voisins");
+                break;
+            }
+            List<Pair<Integer,Integer>> voisins = matrice.get(courant);
+            // Pour chaque voisins on change nos poids de chemin
+            for (Pair<Integer,Integer> v:voisins
+            ) {
+                int v_key = v.getKey();
+                int v_value = v.getValue();
+                int poidsChemin = v_value+poidsCourant;
+                //On change si on ne l'a pas deja parcourus
+                if( !list.containsKey(v_key) && poidsChemin< poids.get(v_key)){
+                    poids.put(v_key,poidsChemin);
+                }
+
+            }
+            //on a parcourus ce sommet
+            list.put(courant,poids.get(courant));
+            poids.remove(courant);
+            int min = Integer.MAX_VALUE;
+            //On cherche le sommet suivant (poids minimum)
+            for(Map.Entry<Integer, Integer> entry : poids.entrySet()) {
+
+                int key = entry.getKey();
+                int value = entry.getValue();
+                System.out.println(min);
+                System.out.println(value);
+
+                if(value < min){
+                    min=value;
+                    courant=key;
+                }
+
+            }
+            poidsCourant=min;
+        }
+        return list;
+    }
+
+
 
     @Override
     public int hashCode() {
@@ -97,107 +278,16 @@ public class GraphQ3 {
     public HashMap<Integer, List<Pair<Integer, Integer>>> getMatrice() {
         return matrice;
     }
-    public List<Pair<Integer, Integer>> getValueMatrice(int key) {
-        return matrice.get(key);
-    }
 
-
-    public Pair<ArrayList<Integer>,Integer> prim(int source){
-
-        //Queue<Integer> queue = new LinkedList<>(matrice.keySet());
-        // L'arbre des sommets a parcourir
-        ArrayList<Integer> tree = new ArrayList<>();
-        // Tous les arcs dont le sommet n'est pas dans tree
-        ArrayList<Pair<Integer,Integer>> allEdges=new ArrayList<>();
-
-        int current =source;
-        tree.add(source);
-        // Poids total
-        int cost = 0;
-        //Tant que on n'a pas tous les sommets dans notre arbre
-        while (tree.size() != matrice.keySet().size()) {
-            // Tous les voisins de current
-            List<Pair<Integer,Integer>> edges = matrice.get(current);
-            // On ajoute tous les voisins
-            boolean goodAdd =   allEdges.addAll(edges);
-            // On supprime ceux dont on a deja un lien
-            allEdges.removeIf(p -> tree.contains(p.getKey()));
-            assert goodAdd = false;
-
-
-            // On trouve le minimum des voisins
-            Optional<Pair<Integer, Integer>> min = allEdges.stream()
-                    .min(Comparator.comparing(Pair::getValue));
-
-            if(min.isPresent()){
-                //On ajoute le poids minimum
-                cost+=min.get().getValue();
-                //On ajoute le sommet dans notre arbre
-                tree.add(min.get().getKey());
-                //on change le prochain current
-                current=min.get().getKey();
-            }
-
+    @Override
+    public String toString() {
+        StringBuilder res= new StringBuilder();
+        HashMap<Integer, List<Pair<Integer, Integer>>> listAdja =this.getMatrice();
+        for(Map.Entry<Integer, List<Pair<Integer, Integer>>> entry : listAdja.entrySet()){
+            int key = entry.getKey();
+            List<Pair<Integer, Integer>> value = entry.getValue();
+            res.append("Le sommet :").append(key).append(" a comme voisins :").append(value.toString()+"\n");
         }
-
-        return new Pair<>(tree,cost);
+        return String.valueOf(res);
     }
-
-    public HashMap dijkstra (int source){
-
-        int poidsCourant = 0;
-        // liste des sommets non parcourus avec le poids du chemin pour y arriver
-        HashMap<Integer,Integer> poids = new HashMap<>();
-        //Liste des sommets parcourus
-        HashMap<Integer,Integer> list = new HashMap<>();
-        //La distance ini est de 0
-        poids.put(source,0);
-        // Tous les sommets
-        Set<Integer> keys = matrice.keySet();
-        //On met infini dans les chemins non parcourus
-        for (int k: keys
-             ) {
-            if(k!=source){
-                poids.put(k,Integer.MAX_VALUE);
-            }
-        }
-
-        int courant = source;
-        // Tant qu'on a pas parcourus tous les sommets
-        while (poids.size() != 0){
-            //On recupere les voisins du courant
-            List<Pair<Integer,Integer>> voisins = matrice.get(courant);
-            // Pour chaque voisins on change nos poids de chemin
-            for (Pair<Integer,Integer> v:voisins
-                 ) {
-                int v_key = v.getKey();
-                int v_value = v.getValue();
-                int poidsChemin = v_value+poidsCourant;
-                //On change si on ne l'a pas deja parcourus
-                if( !list.containsKey(v_key) && poidsChemin< poids.get(v_key)){
-                    poids.put(v_key,poidsChemin);
-                }
-
-            }
-            //on a parcourus ce sommet
-            list.put(courant,poids.get(courant));
-            poids.remove(courant);
-            int min = Integer.MAX_VALUE;
-            //On cherche le sommet suivant (poids minimum)
-            for(Map.Entry<Integer, Integer> entry : poids.entrySet()) {
-
-                int key = entry.getKey();
-                int value = entry.getValue();
-                if(value < min){
-                    min=value;
-                    courant=key;
-                }
-
-            }
-            poidsCourant=min;
-        }
-
-        return list;
-    }
-
 }
--- src/main/java/Graph/Main.java
+++ src/main/java/Graph/Main.java
@@ -1,99 +1,55 @@
 package Graph;
 
-import Graph.Q1.Graph;
-import Graph.Q2.GraphQ2;
-import Graph.Q3.GraphQ3;
 import javafx.util.Pair;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("\n===================== Question 1 =====================");
         Graph graph =creationGrapheQ1(5);
         int nb = graph.getNbConnectedComp();
-        HashMap<Integer, List<Integer>> listAdja =graph.getListAdjacency();
-        for(Map.Entry<Integer, List<Integer>> entry : listAdja.entrySet()){
-            int key = entry.getKey();
-            List<Integer> value = entry.getValue();
-            System.out.println("Le sommet :"+key+" a comme voisins :"+ value.toString());
-        }
         System.out.println("Number of connected component(s) : "+nb);
-        boolean isC = true;
-        if(nb>1){
-            isC = false;
-        }
-        System.out.println("Is the graph connected ? "+isC);
-        /*
+        System.out.println(graph.toString());
+        System.out.println("Is the graph connected ? "+graph.estCo(nb));
+
         System.out.println("\n===================== Question 2 =====================");
-        GraphQ2 graphQ2= creationGrapheQ2(5);
-        System.out.println(graphQ2.getMatrice());
+        int nbSommets = 5;
+        System.out.println("Le nombred le sommets est = " + nbSommets);
+        int source = (int) (Math.random()* (nbSommets-1));
+        System.out.println("la source est = "+ source);
+        Graph graphQ2= creationGrapheQ2Q3(nbSommets,false);
+        System.out.println(graphQ2.toString());
 
-        if(!graph2toGraph1(graphQ2)){
-            System.out.println("Ca va bugger");
-        }else{
-            Pair<ArrayList<Integer>,Integer> res= graphQ2.prim(1);
-            System.out.println(res.getKey().toString());
-            System.out.println(res.getValue());
-        };
-
-        /*
-
-
+        Pair<ArrayList<Integer>,Integer> res= graphQ2.prim(source);
+        if(res != null){
+            System.out.println("L'arbre couvrant minimale est :" + res.getKey().toString());
+            System.out.println("Le poids :" +res.getValue());
+        }
 
 
-        graphQ2.addNode(1);
-        //A
-        graphQ2.addNextToNode(1,2,2);
-        graphQ2.addNextToNode(1,3,3);
-        graphQ2.addNextToNode(1,4,3);
-        //B
-        graphQ2.addNextToNode(2,3,4);
-        graphQ2.addNextToNode(2,5,3);
-        //C
-        graphQ2.addNextToNode(3,4,5);
-        graphQ2.addNextToNode(3,5,1);
-        graphQ2.addNextToNode(3,6,6);
-        //D
-        graphQ2.addNextToNode(4,6,7);
-        //E
-        graphQ2.addNextToNode(5,6,8);
-        //F
-        graphQ2.addNextToNode(6,7,9);
+        System.out.println(" \n===================== Question 3 =====================");
+        Graph graphQ3= creationGrapheQ2Q3(nbSommets,true);
+        int nbSommetsQ3 = 5;
+        System.out.println("Le nombred le sommets est = " + nbSommetsQ3);
+        int sourceQ3 = (int) (Math.random()* (nbSommets-1));
+        System.out.println("la source est = "+ sourceQ3);
 
-        Pair<ArrayList<Integer>,Integer> res= graphQ2.prim(1);
-        System.out.println(res.getKey().toString());
-        System.out.println(res.getValue());
+        System.out.println(graphQ3.toString());
+        HashMap<Integer,Integer> resDij = graphQ3.dijkstra(sourceQ3);
+        if(resDij !=null){
+            AtomicInteger poids = new AtomicInteger();
+            resDij.forEach((k,v)-> poids.addAndGet((Integer) v));
+            System.out.println(resDij.toString());
+            System.out.println(poids);
+        }else {
+            System.out.println("Source sans voisins");
+        }
 
-        System.out.println("Question 2 =====================");
-        GraphQ3 graphQ3= new GraphQ3();
-
-        graphQ3.addNode(1);
-        graphQ3.addNode(2);
-        graphQ3.addNode(3);
-        graphQ3.addNode(4);
-        graphQ3.addNode(5);
-
-        graphQ3.addNextToNode(1,5,3);
-        graphQ3.addNextToNode(1,3,18);
-        graphQ3.addNextToNode(5,2,10);
-        graphQ3.addNextToNode(5,4,2);
-        graphQ3.addNextToNode(4,2,1);
-        graphQ3.addNextToNode(2,3,4);
-        graphQ3.addNextToNode(2,1,8);
-
-        HashMap resDij = graphQ3.dijkstra(1);
-        AtomicInteger poids = new AtomicInteger();
-        resDij.forEach((k,v)-> poids.addAndGet((Integer) v));
-
-        System.out.println(resDij.toString());
-        System.out.println(poids);
-
-         */
 
 
 
@@ -113,11 +69,10 @@ public class Main {
             while (!bonArc){
                 int source = (int) (Math.random()*(nbSommets));
                 int destination = (int) (Math.random()*(nbSommets));
-                HashMap<Integer, List<Integer>> listAdja = graph.getListAdjacency();
+                HashMap<Integer, List<Pair<Integer, Integer>>> listAdja = graph.getMatrice();
                 if(!listAdja.get(source).contains(destination) && source!=destination){
                     bonArc=true;
-                    graph.addNextToNode(source,destination);
-
+                    graph.addNextToNodeNoWeight(source,destination);
                 }
             }
         }
@@ -125,8 +80,8 @@ public class Main {
         return graph;
     }
 
-    public static GraphQ2 creationGrapheQ2(int nbSommets){
-        GraphQ2 graph = new GraphQ2();
+    public static Graph creationGrapheQ2Q3(int nbSommets,boolean oriented){
+        Graph graph = new Graph();
 
         //(n*(n-1) ) /2
         for (int i = 0; i < nbSommets; i++) {
@@ -134,7 +89,6 @@ public class Main {
         }
 
         int nbArcs=(int) (Math.random()* (nbSommets*(nbSommets-1))/2);
-        System.out.println(nbArcs);
         for (int i = 0; i < nbArcs; i++) {
             boolean bonArc=false;
             while (!bonArc){
@@ -144,7 +98,6 @@ public class Main {
                 if(source!=destination){
                     HashMap<Integer, List<Pair<Integer, Integer>>> matrice = graph.getMatrice();
                     List<Pair<Integer, Integer>> voisins= matrice.get(source);
-                    int j =0;
                     boolean voisinsTrouve = false;
                     for (Pair<Integer, Integer> v: voisins
                     ) {
@@ -155,7 +108,7 @@ public class Main {
                     if(!voisinsTrouve ){
                         bonArc=true;
                         int poids = (int) (Math.random()*(19))+1;
-                        graph.addNextToNode(source,new Pair<>(destination,poids));
+                        graph.addNextToNodeWithWeight(source,new Pair<>(destination,poids),oriented);
                     }
                 }
 
@@ -165,34 +118,6 @@ public class Main {
         return graph;
     }
 
-    public static boolean graph2toGraph1(GraphQ2 g2){
-        HashMap<Integer, List<Pair<Integer, Integer>>> matrice = g2.getMatrice();
-        Graph g= new Graph();
-        for(Map.Entry<Integer,  List<Pair<Integer, Integer>>> entry : matrice.entrySet()) {
 
-            int key = entry.getKey();
-            List<Pair<Integer, Integer>> values = entry.getValue();
-            for (Pair<Integer, Integer> p: values
-                 ) {
-                g.addNextToNode(key,p.getKey());
-            }
-
-        }
-
-        int nb = g.getNbConnectedComp();
-        System.out.println("Number of connected component(s) : "+nb);
-        boolean isC = true;
-        if(nb>1){
-            isC = false;
-        }
-        HashMap<Integer, List<Integer>> listAdja =g.getListAdjacency();
-        for(Map.Entry<Integer, List<Integer>> entry : listAdja.entrySet()){
-            int key = entry.getKey();
-            List<Integer> value = entry.getValue();
-            System.out.println("Le sommet :"+key+" a comme voisins :"+ value.toString());
-        }
-
-        return isC;
-    }
 
 }
